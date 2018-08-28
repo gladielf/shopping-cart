3f0ca0cfeb40bef4c8a7835cfa25747bea50b673
Unknown <gladielf@gmail.com>
2018-08-28 22:57:53 +0200

    Vuex Getters

--- src/api/shop.js
+++ src/api/shop.js
@@ -6,12 +6,12 @@ const _products = [
     {"id": 2, "title": "H&M T-Shirt White", "price": 10.99, "inventory": 10},
     {"id": 3, "title": "Charli XCX - Sucker CD", "price": 19.99, "inventory": 5}
   ]
-  
+
   export default {
     getProducts (cb) {
       setTimeout(() => cb(_products), 100)
     },
-  
+
     buyProducts (products, cb, errorCb) {
       setTimeout(() => {
         // simulate random checkout failure.
