26ebde07af0ad88356a93b10ccbf881287bd88e3
Unknown <gladielf@gmail.com>
2018-09-01 23:37:57 +0200

    Store Access from all Components

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-    import store from '@/store/index'
     export default {
         data() {
             return {
@@ -23,13 +22,13 @@
         },
         computed: {
             products () {
-                return store.getters.availableProducts
+                return this.$store.getters.availableProducts
             }
         },
 
         created () {
             this.loading = true
-            store.dispatch('fetchProducts').then(() => this.loading = false)
+            this.$store.dispatch('fetchProducts').then(() => this.loading = false)
         }
     }
 </script>
