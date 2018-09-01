1d4d183b1c660ec131446576628e57b3f3a6d201
Unknown <gladielf@gmail.com>
2018-09-01 23:35:35 +0200

    Vuex Actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
@@ -15,8 +16,17 @@ export default new Vuex.Store({
     },
 
     actions: {
-        fetchProducts () {
-            // make the call
+        fetchProducts ({commit}) {
+
+            return new Promise((resolve, reject) => {
+                // make the call
+                shop.getProducts( products => {
+                    commit('setProducts', products)
+                    resolve()
+                })
+
+            });
+
         }
     },
 
